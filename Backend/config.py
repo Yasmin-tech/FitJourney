84b41dde2712a93d57385ef92283a6324177cbb7
Yasmin Mahmud <jazzsalam2020@gmail.com>
2024-12-19 12:00:30 +0300

    create the configuration file
    configure SQLALCHEMY_DATABASE_URI

---
+++ Backend/config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+    The configuration file for the flask app
+    """
+
+
+from dotenv import load_dotenv
+from os import getenv
+
+
+load_dotenv() # Load environment variables from .env file
+
+
+class Config():
+    """
+        The base configuration class """
+
+    # Set default values for environment variables if they are not provided
+
+    MYSQL_USERNAME = getenv("MYSQL_USERNAME", "fit_journey_user")
+    MYSQL_PASSWORD = getenv("MYSQL_PASSWORD", "my_passowrd")
+    MYSQL_DB = getenv("MYSQL_DB", "FitJourney")
+    MYSQL_HOST = getenv("MYSQL_HOST", "localhost")
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlclient://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
